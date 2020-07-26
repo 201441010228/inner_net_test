e362a0df1d506fe1e3be274118dbd6ffe51db984
zy <1443356733@qq.com>
2020-07-27 00:10:20 +0800

    feat(内网穿透)：初始化提交

---
+++ inner_net/src/main/java/com/zy/test/inner_net/controller/FindEatDinner.java
@@ -0,0 +1,27 @@
+package com.zy.test.inner_net.controller;
+
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Random;
+
+@RestController
+public class FindEatDinner {
+
+
+    private static final Map<Integer, Long> map = new HashMap<>(16);
+
+    @RequestMapping(value = "please_click")
+    private Object findEatDinnerPeople() {
+        return "我想你了";
+    }
+
+    @RequestMapping(value = "random")
+    private Object random() {
+       Random r = new Random();
+       return "这次随机数是" + r.nextInt(10);
+    }
+}
